40fcbc46373ce99e18564ab4f952d92a605d7cc2
Pavel Bisse <pavel@billomat.com>
2020-05-22 22:41:26 +0200

    move middlewares to a separate file

--- api/middlewares.go
+++ api/middlewares.go
@@ -5,6 +5,27 @@ import (
 	"net/http"
 )
 
+func basicAuthMiddleware(user, pass string, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if checkBasicAuth(r, user, pass) {
+			next(w, r)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		_, _ = w.Write([]byte("Unauthorized"))
+	}
+}
+
+func checkBasicAuth(r *http.Request, user, pass string) bool {
+	u, p, ok := r.BasicAuth()
+	if !ok {
+		return false
+	}
+	return u == user && p == pass
+}
+
 var ContentTypeErr = errors.New("Content-Type header must be application/json; charset=utf-8")
 var AcceptErr = errors.New("accept header must be application/json; charset=utf-8")
 
